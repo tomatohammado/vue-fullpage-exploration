2422359119d27dcc7465006a5f55949e2d3ac59c
Hammad Malik <hmd.node@gmail.com>
2018-02-21 09:58:42 -0500

    Attempt to get vue-fullpage to work
    So far, not so great...

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul id="menu">
+      <li data-menuanchor="week-1" class="active"><a href="#week-1">Week 1</a></li>
+      <li data-menuanchor="week-2"><a href="#week-2">Week 2</a></li>
+      <li data-menuanchor="week-3"><a href="#week-3">Week 3</a></li>
+    </ul>
+
+
+    <full-page :options="options" id="fullpage">
+      <div class="section">
+        <h3>Section 1</h3>
+      </div>
+      <div class="section">
+        <div class="slide">
+           <h3>Slide 2.1</h3>
+        </div>
+        <div class="slide">
+           <h3>Slide 2.2</h3>
+        </div>
+        <div class="slide">
+           <h3>Slide 2.3</h3>
+        </div>
+      </div>
+      <div class="section">
+         <h3>Section 3</h3>
+      </div>
+    </full-page>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import FullPage from './components/FullPage.vue'
+  import fullPageMixin from './fullPageMixin'
 
 export default {
   name: 'app',
+  mixins: [fullPageMixin],
   components: {
-    HelloWorld
-  }
+    FullPage
+  },
+  data() {
+    return {
+      options: {
+        scrollBar: false,
+        menu: '#menu',
+        navigation: true,
+        anchors: ['week-1', 'week-2', 'week-3']
+      },
+    }
+  },
 }
 </script>
 
---
+++ src/components/FullPage.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import * as constants from '../constants';
+  import {
+    camelToKebab,
+  } from '../helpers';
+
+/**
+ * vue-fullpage.js
+ * https://github.com/alvarotrigo/vue-fullpage.js
+ * @license https://github.com/alvarotrigo/vue-fullpage.js#license
+ *
+ * Copyright (C) 2018 alvarotrigo.com - A project by Alvaro Trigo
+ */
+  export default {
+    props: {
+      options: {
+        type: Object,
+        default: () => {
+          return {};
+        },
+      },
+    },
+
+    data() {
+      return {
+        events: constants.EVENTS.reduce((eventsHandlers, event) => {
+          return {
+            ...eventsHandlers,
+
+            [event]: (...args) => {
+              this.emitEvent(event, args);
+            }
+          };
+        }, {}),
+      }
+    },
+
+    watch: {
+      options: {
+        deep: true,
+        handler() {
+          this.update();
+        },
+      },
+    },
+
+    mounted() {
+      this.update();
+    },
+
+    methods: {
+      destroy() {
+        if(typeof $.fn.fullpage.destroy !== 'undefined'){
+            $.fn.fullpage.destroy('all');
+        }
+      },
+
+      emitEvent(name, args) {
+        // Emit event on Vue way
+        this.$emit.apply(this, [camelToKebab(name), ...args]);
+
+        // Run event's handler with non Vue way
+        if (this.options.hasOwnProperty(name)) {
+          this.options[name].apply(this, args);
+        }
+      },
+
+      update() {
+        var slideSelector = this.options.slideSelector ? this.options.slideSelector : '.slide';
+        var sectionSelector = this.options.sectionSelector ? this.options.sectionSelector : '.section';
+        let activeSectionIndex = $('.fp-section.active').index();
+        let activeSlideIndex = $('.fp-section.active').find('.fp-slide.active').index();
+
+        this.destroy('all');
+
+        if(activeSectionIndex > -1){
+          $(sectionSelector).eq(activeSectionIndex).addClass('active');
+        }
+
+        if(activeSlideIndex > -1){
+          $(sectionSelector + '.active').find(slideSelector).eq(activeSlideIndex).addClass('active');
+        }
+        $(this.$el).fullpage({
+          ...this.options,
+          ...this.events,
+        });
+      },
+    },
+  };
+</script>
